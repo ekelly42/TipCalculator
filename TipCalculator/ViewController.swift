4e85d4fa31e2b08119ab505c4d4ce0c5094a9a9e
Eric Kelly <ekellyhawks@gmail.com>
2022-08-18 23:06:36 -0500

    Add basic text elements to screen

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var BillAmountTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-// 
+    @IBAction func calculateTip(_ sender: Any) {
+        // get bill amount
+//        let bill = Double(BillAmountTextField.text!) ?? 0
+//
+        // get tip by multiplying bill * tip %
+        
+    }
+
 }
 
